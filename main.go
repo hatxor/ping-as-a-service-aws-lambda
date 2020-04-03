5d77c2e262c4f3240a2a6079610c75f33f4da472
Axel Amigo Arnold <axl89@users.noreply.github.com>
2020-04-03 17:23:24 +0200

    Fixed latency negative overflow

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/tcnksm/go-httpstat"
 )
 
-type UrlConfig struct {
+type URLConfig struct {
 	ID         int    `json:"id"`
 	URL        string `json:"url"`
 	ExpectCode int    `json:"expected_code"`
@@ -27,10 +27,10 @@ type UrlConfig struct {
 }
 
 type MyEvent struct {
-	URLs []UrlConfig `json:"urls"`
+	URLs []URLConfig `json:"urls"`
 }
 
-type UrlResult struct {
+type URLResult struct {
 	ID                 int    `json:"id"`
 	ReceivedCode       int    `json:"received_code"`
 	ExceededMaxTimeOut bool   `json:"exceeded_max_timeout"`
@@ -39,17 +39,21 @@ type UrlResult struct {
 }
 
 type MyResponse struct {
-	URLs []UrlResult `json:"urls"`
+	URLs []URLResult `json:"urls"`
+}
+
+func getTotalTime(r *httpstat.Result) time.Duration {
+	return r.Connect + r.DNSLookup + r.NameLookup + r.Pretransfer + r.ServerProcessing + r.StartTransfer + r.TCPConnection + r.TLSHandshake
 }
 
 // Status makes a GET request to a given URL and checks whether or not the
 // resulting status code is 200.
-func Status(siteUrl UrlConfig, wg *sync.WaitGroup) {
+func Status(siteURL URLConfig, wg *sync.WaitGroup) {
 
 	defer wg.Done()
 
 	// Create a new HTTP request
-	req, err := http.NewRequest("GET", siteUrl.URL, nil)
+	req, err := http.NewRequest("GET", siteURL.URL, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,10 +63,13 @@ func Status(siteUrl UrlConfig, wg *sync.WaitGroup) {
 	req = req.WithContext(ctx) // Send request by default HTTP client
 	client := http.DefaultClient
 	resp, err := client.Do(req)
+
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	defer resp.Body.Close()
 
 	if _, err := io.Copy(ioutil.Discard, resp.Body); err != nil {
 		log.Fatal(err)
@@ -70,43 +77,39 @@ func Status(siteUrl UrlConfig, wg *sync.WaitGroup) {
 
 	var score int = 0
 	var match int = 0
-	var response UrlResult
-	response.ID = siteUrl.ID
+	var response URLResult
+	response.ID = siteURL.ID
 	response.ReceivedCode = 0
 	response.ExceededMaxTimeOut = false
 	response.ContentFound = false
 	response.Error = "false"
-	var now = time.Now()
 
 	if err != nil {
-		//response.Error = fmt.Sprintf("%v", err)
 		if err2, ok := err.(*url.Error); ok {
 			if err3, ok := err2.Err.(net.Error); ok {
 				response.ExceededMaxTimeOut = err3.Timeout()
 			}
 		}
-	} else {
-
-		response.ReceivedCode = resp.StatusCode
-
-		defer resp.Body.Close()
-
-		data, err := ioutil.ReadAll(resp.Body)
-		if err == nil {
-			response.ContentFound = strings.Contains(string(data), siteUrl.Content)
-		}
-
-		if response.ContentFound == true {
-			match = 1
-		}
-
-		if resp.StatusCode == siteUrl.ExpectCode {
-			score = 1
-		}
-
+		return
 	}
 
-	var total_time = result.Total(now)
+	response.ReceivedCode = resp.StatusCode
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err == nil {
+		response.ContentFound = strings.Contains(string(data), siteURL.Content)
+	}
+
+	if response.ContentFound == true {
+		match = 1
+	}
+
+	if resp.StatusCode == siteURL.ExpectCode {
+		score = 1
+	}
+
+	var totalTime = getTotalTime(&result)
 
 	/**
 	* u: url
@@ -116,7 +119,7 @@ func Status(siteUrl UrlConfig, wg *sync.WaitGroup) {
 	* r: region
 	* t: total request time duration (in ms)
 	 */
-	hackalogURL := fmt.Sprintf("https://hackalog.scalefast.ninja/i?u=%s&i=%s&s=%s&m=%s&r=%s&t=%d", siteUrl.URL, strconv.Itoa(siteUrl.ID), strconv.Itoa(score), strconv.Itoa(match), os.Getenv("AWS_REGION"), int(total_time/time.Millisecond))
+	hackalogURL := fmt.Sprintf("https://hackalog.scalefast.ninja/i?u=%s&i=%s&s=%s&m=%s&r=%s&t=%d", siteURL.URL, strconv.Itoa(siteURL.ID), strconv.Itoa(score), strconv.Itoa(match), os.Getenv("AWS_REGION"), int(totalTime/time.Millisecond))
 
 	var httpClient = &http.Client{
 		Timeout: time.Second * 5,
@@ -132,7 +135,7 @@ func Status(siteUrl UrlConfig, wg *sync.WaitGroup) {
 	}*/
 }
 
-func checkURLsStatus(URLs []UrlConfig) {
+func checkURLsStatus(URLs []URLConfig) {
 	var wg sync.WaitGroup
 	for _, url := range URLs {
 		wg.Add(1)
